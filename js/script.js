d794877c4bd6ed0957e94a7513f0604c6d4e7169
Milton <milleung964@gmail.com>
2017-05-11 22:43:21 -0700

    Add sliding

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    var currentIndex = 0;
+    document.getElementById("home-link").focus();
+    
+    $("#navbar-list li").click(function() {
+        var list = Array.prototype.slice.call( document.getElementById('navbar-list').children );
+        var index = list.indexOf(this);
+        $("#carousel").carousel(index);
+    });
+    
+    $("#left-carousel").click(function() {
+        if (currentIndex == 0) {
+            currentIndex = 3;
+        } else {
+            currentIndex = currentIndex - 1;   
+        }
+        setSelected(currentIndex);
+    });
+    
+    $("#right-carousel").click(function() {
+        if (currentIndex == 3) {
+            currentIndex = 0;
+        } else {
+            currentIndex = currentIndex + 1;   
+        }
+        setSelected(currentIndex);
+    });
+    
+    function setSelected(index) {
+        var id;
+        switch (index) {
+            case 0:
+                id = "home-link";
+                break;
+            case 1:
+                id = "services-link";
+                break;
+            case 2: 
+                id = "about-link";
+                break;
+            case 3:
+                id = "contact-link";
+                break;
+        }
+        document.getElementById(id).focus();
+    }
+});
